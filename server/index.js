1379a2ec56b3aff3edc69f872dbf13bf29c29288
natashaDianeey <ndianeey@gmail.com>
2020-08-12 22:24:07 -0500

    add cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT;
 const router = require('../routes');
 const fileUpload = require('express-fileupload');
-
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
@@ -11,6 +11,7 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
 }))
+app.use(cors());
 app.use('/api/v1', router);
 
 
